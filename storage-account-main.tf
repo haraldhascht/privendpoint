c12e11977476ac9104b00d259ab5172259b4fc7e
Harald Haschtschek <harald.haschtschek@sits-d.de>
2023-01-05 13:53:38 +0100

    net access to false

--- storage-account-main.tf
+++ storage-account-main.tf
@@ -29,7 +29,7 @@ resource "azurerm_storage_account" "storage" {
   account_kind             = "StorageV2"
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  public_network_access_enabled = true
+  public_network_access_enabled = false
 }
 
 
